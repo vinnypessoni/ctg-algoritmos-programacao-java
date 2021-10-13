16b41d082382a7ea6ff75d10c0986fc8f5eeee2d
viniciuspessoni <Ch@conete7>
2021-10-13 14:37:33 +0100

    Atualização exemplo

--- src/main/java/AlgoProgAula_2.java
+++ src/main/java/AlgoProgAula_2.java
@@ -1,5 +1,11 @@
 public class AlgoProgAula_2 {
     public static void main(String args[]){
-        System.out.println("Hey aula 2 CTG");
+
+        int numeroAula = 2;
+        String mensagemBoasVindas = "CTG - Bem vindos a aula";
+
+        System.out.println(mensagemBoasVindas + " ---- " + numeroAula);
     }
 }
+
+
